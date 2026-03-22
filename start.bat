4451e4285ed68aa2f954b555af14c45d814cf9db
umino <deryazhang@163.com>
2026-03-23 00:27:53 +0800

    Update bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-start cmd /k "cd /d \"%~dp0\" && python app.py"
 start cmd /k "cd /d \"%~dp0\" && ngrok http --domain=glenn-toothier-beulah.ngrok-free.dev 5000"
+python app.py
