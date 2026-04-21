69b2e11e68efc0d35fa9a3aaf189892348148e22
umino <deryazhang@163.com>
2026-04-21 21:23:25 +0800

    Clear proxy for ngrok

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
 start "Flask" cmd /k "python app.py"
-start "ngrok" cmd /k "ngrok http --domain=glenn-toothier-beulah.ngrok-free.dev 5000"
+start "ngrok" cmd /k "set http_proxy=&& set https_proxy=&& set HTTP_PROXY=&& set HTTPS_PROXY=&& ngrok http --url=glenn-toothier-beulah.ngrok-free.dev 5000"
